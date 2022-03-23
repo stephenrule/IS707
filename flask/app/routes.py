5ae56adf3fae9afb6030ac548c438d4955021e0d
stephenrule <stephen.rule01@gmail.com>
2022-03-23 12:20:07 -0400

    Updated links and rearranged things

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -1,8 +1,13 @@
 from turtle import hideturtle
-from flask import render_template, flash, redirect
+from flask import request, render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
+# Import local python files
+import db
+import chat
+
+# Main Webpage - Splash Page
 @app.route('/')
 @app.route('/index')
 def index():
@@ -10,8 +15,8 @@ def index():
     user = {'username': 'user'}
     posts = [
         {
-        'author': {'username': 'John'},
-        'body': 'Beautiful day in portland!'
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in portland!'
         },
         {
             'author': {'username': 'Susan'},
@@ -21,135 +26,25 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-##########################  DB  ###########################
-#### db.py
-import db
-
-########################## CHAT ###########################
-#### long_responses.py
-import long_responses as long
-import random
-
-R_EATING = "I don't like eating anything because I'm a bot obviously!"
-R_ADVICE = "If I were you, I would go to the internet and type exactly what you wrote there!"
-
-def unknown():
-    response = ['Could you please re-phrase that?', 
-                "...", 
-               "Sounds about right",
-               "What does that mean?"][random.randrange(4)]
-    return response
-
-
-#### Chat
-from flask import Flask, render_template, request
-import re
-# Not pulling in long_responses for some reason. Run flask shell and error popped up
-# Fix: Copied contents from long_responses into this file. 
-#import long_responses as long
-
-def message_probability(user_message, recognised_words, single_response=False, required_words=[]):
-    message_certainty = 0
-    has_required_words = True
-
-    # Counts how many words are present in each predefined message
-    for word in user_message:
-        if word in recognised_words:
-            message_certainty += 1
-
-    # Calculates the percent of recognised words in a user message
-    percentage = float(message_certainty) / float(len(recognised_words))
-
-    # Checks that the required words are in the string
-    for word in required_words:
-        if word not in user_message:
-            has_required_words = False
-            break
-
-    # Must either have the required words, or be a single response
-    if has_required_words or single_response:
-        return int(percentage * 100)
-    else:
-        return 0
-    
-def check_all_messages(message):
-    highest_prob_list = {}
-
-    # Simplifies response creation / adds it to the dict
-    def response(bot_response, list_of_words, single_response=False, required_words=[]):
-        nonlocal highest_prob_list
-        highest_prob_list[bot_response] = message_probability(message, list_of_words, single_response, required_words)
-
-    #login = "Hackers Storage Unit Login Page - <a href=https://www.google.com>Login Page</a>"
-    login = "UMBC Storage: <a href=/login>Login Page</a>"
-    site_map = "<img src=\"static/site_map.png\" alt=\"Site Map Image\">"
-    contact = "<img src=\"static/contact.png\" alt=\"Contact Image\">"
-
-    # Responses -------------------------------------------------------------------------------------------------------
-    # NOTE: Look into stemming and lemmatizing
-    # NOTE: NLTK corpus (For narrowing hello, hi, hey, etc): from nltk.corpus import wordnet
-    # NOTE: syns = wordnet.synsets
-    response('Hello!', ['hello', 'hi', 'hey', 'sup', 'heyo'], single_response=True)
-    response('See you!', ['bye', 'goodbye'], single_response=True)
-    response('I\'m doing fine, and you?', ['how', 'are', 'you', 'doing'], required_words=['how'])
-    response('You\'re welcome!', ['thank', 'thanks'], single_response=True)
-    response('Thank you!', ['i', 'love', 'code', 'palace'], required_words=['code', 'palace'])
-    response(login, ['website', 'login'], single_response=True)  
-    response(site_map, ['sitemap', 'site', 'map'], single_response=True)
-    response(contact, ['contact', 'number', 'address', 'phone'], single_response=True)
-    response('Small Storage Unit Price: $50', ['small', 'storage', 'unit', 'price'], required_words=['small', 'price'])
-    response('Medium Storage Unit Price: $75', ['medium', 'storage', 'unit', 'price'], required_words=['medium', 'price'])
-    response('Large Storage Unit Price: $95', ['large', 'storage', 'unit', 'price'], required_words=['large', 'price'])
-    response('Small Units Available: ' + str(db.getUnitAvailability('SMALL', 'catonsville')), ['small', 'unit', 'units', 'available'], single_response=True)
-    response(db.getUnitAvailability('MEDIUM', 'catonsville'), ['medium', 'unit', 'units', 'available'], single_response=True)
-    response('Small Units Available: ' + str(db.getUnitAvailability('SMALL', 'catonsville')) + 
-             ', Medium Units Available: ' + str(db.getUnitAvailability('MEDIUM', 'catonsville')) +
-             ', Large Units Available: ' + str(db.getUnitAvailability('Large', 'catonsville'))
-             , ['test', 'unit', 'units', 'available'], single_response=True)
-
-    # Longer responses
-    response(R_ADVICE, ['give', 'advice'], required_words=['advice'])
-    response(R_EATING, ['what', 'you', 'eat'], required_words=['you', 'eat'])
-
-    best_match = max(highest_prob_list, key=highest_prob_list.get)
-    # DEBUG - Shows us the probability
-    for key, value in highest_prob_list.items():
-        print(key, ' : ', value)
-
-    #print(highest_prob_list)
-    
-    #return best_match
-    return unknown() if highest_prob_list[best_match] < 1 else best_match
-
-
-# Used to get the response
-def get_response(user_input):
-    split_message = re.split(r'\s+|[,;?!.-]\s*', user_input.lower())
-    ####################
-    # Remove stop words - nltk
-    #####################
-    response = check_all_messages(split_message)
-    return response
-
-
-# Testing the response system
-#while True:
-#    print('Bot: ' + get_response(input('You: ')))
-####################################################################
-
+# Called from the chatbot page (chat.html - "/get") - $.get("/get", { msg: rawText }).done(function(data) {
+# Takes user input and gets chatbot response from chat (chat.py) to be displayed on Chatbot Page
 @app.route('/get')
 def get_bot_response():
     userText = request.args.get('msg')
-    return str(get_response(userText))
+    return str(chat.get_response(userText))
 
+# Chatbot Page
 @app.route('/chat')
 def home():
     return render_template("chat.html")
 
-@app.route('/cha')
-def homea():
-    return render_template("cha.html")
+# Testing Page - Temporary and used for testing things. 
+# Note: Remove link in base.html and delete test.html when complete
+@app.route('/test')
+def test1():
+    return render_template("test.html")
 
+# User Login Page
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
---
+++ flask/chat.py
@@ -0,0 +1,99 @@
+# Database: db.py
+import db
+
+# Responses: long_responses.py
+import long_responses as long
+
+# Imports
+import random
+from flask import Flask, render_template, request
+import re
+
+
+#### Determines probability based on number of words that match and returns highest value (Percentage)
+def message_probability(user_message, recognised_words, single_response=False, required_words=[]):
+    message_certainty = 0
+    has_required_words = True
+
+    # Counts how many words are present in each predefined message
+    for word in user_message:
+        if word in recognised_words:
+            message_certainty += 1
+
+    # Calculates the percent of recognised words in a user message
+    percentage = float(message_certainty) / float(len(recognised_words))
+
+    # Checks that the required words are in the string
+    for word in required_words:
+        if word not in user_message:
+            has_required_words = False
+            break
+
+    # Must either have the required words, or be a single response
+    if has_required_words or single_response:
+        return int(percentage * 100)
+    else:
+        return 0
+
+# Take user input and see if any word matches predefined text
+def check_all_messages(message):
+    highest_prob_list = {}
+
+    # Simplifies response creation / adds it to the dict
+    def response(bot_response, list_of_words, single_response=False, required_words=[]):
+        nonlocal highest_prob_list
+        highest_prob_list[bot_response] = message_probability(message, list_of_words, single_response, required_words)
+
+    # Images and Links
+    #login = "Hackers Storage Unit Login Page - <a href=https://www.google.com>Login Page</a>"
+    login = "UMBC Storage: <a href=/login>Login Page</a>"
+    site_map = "<img src=\"static/site_map.png\" alt=\"Site Map Image\">"
+    contact = "<img src=\"static/contact.png\" alt=\"Contact Image\">"
+
+    # Responses -------------------------------------------------------------------------------------------------------
+    # NOTE: Look into stemming and lemmatizing
+    # NOTE: NLTK corpus (For narrowing hello, hi, hey, etc): from nltk.corpus import wordnet
+    # NOTE: syns = wordnet.synsets
+    response('Hello!', ['hello', 'hi', 'hey', 'sup', 'heyo'], single_response=True)
+    response('See you!', ['bye', 'goodbye'], single_response=True)
+    response('I\'m doing fine, and you?', ['how', 'are', 'you', 'doing'], required_words=['how'])
+    response('You\'re welcome!', ['thank', 'thanks'], single_response=True)
+    response('Thank you!', ['i', 'love', 'code', 'palace'], required_words=['code', 'palace'])
+    response(login, ['website', 'login'], single_response=True)  
+    response(site_map, ['sitemap', 'site', 'map'], single_response=True)
+    response(contact, ['contact', 'number', 'address', 'phone'], single_response=True)
+    response('Small Storage Unit Price: $50', ['small', 'storage', 'unit', 'price'], required_words=['small', 'price'])
+    response('Medium Storage Unit Price: $75', ['medium', 'storage', 'unit', 'price'], required_words=['medium', 'price'])
+    response('Large Storage Unit Price: $95', ['large', 'storage', 'unit', 'price'], required_words=['large', 'price'])
+    response('Small Units Available: ' + str(db.getUnitAvailability('SMALL', 'catonsville')), ['small', 'unit', 'units', 'available'], single_response=True)
+    
+    # Database responses
+    response(db.getUnitAvailability('MEDIUM', 'catonsville'), ['medium', 'unit', 'units', 'available'], single_response=True)
+    response('Small Units Available: ' + str(db.getUnitAvailability('SMALL', 'catonsville')) + 
+             ', Medium Units Available: ' + str(db.getUnitAvailability('MEDIUM', 'catonsville')) +
+             ', Large Units Available: ' + str(db.getUnitAvailability('Large', 'catonsville'))
+             , ['test', 'unit', 'units', 'available'], single_response=True)
+
+    # Longer responses - long_responses.py
+    response(long.R_ADVICE, ['give', 'advice'], required_words=['advice'])
+    response(long.R_EATING, ['what', 'you', 'eat'], required_words=['you', 'eat'])
+    
+    best_match = max(highest_prob_list, key=highest_prob_list.get)
+    # DEBUG - Shows us the probability for each response in a new line
+    for key, value in highest_prob_list.items():
+        print(key, ' : ', value)
+    # DEBUG - Shows us the probability all together
+    #print(highest_prob_list)
+    
+    #return best_match
+    return long.unknown() if highest_prob_list[best_match] < 1 else best_match
+
+
+# Used to get the response
+def get_response(user_input):
+    split_message = re.split(r'\s+|[,;?!.-]\s*', user_input.lower())
+    ####################
+    # Remove stop words - Use nltk
+    #####################
+    response = check_all_messages(split_message)
+    return response
