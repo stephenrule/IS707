8f739afc3e1fa18843aea36e9126f332fa0d6dd8
Stephen Rule <71147265+stephenrule@users.noreply.github.com>
2022-05-04 12:16:00 -0400

    Update deploy_to_aws.sh

--- deploy_to_aws.sh
+++ deploy_to_aws.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo 'Starting to Deploy...'
-ssh ${{ secrets.USERNAME }}@${{ secrets.HOST_DNS }}
+ssh ec2-user@ec2-34-204-48-212.compute-1.amazonaws.com
 
 cd /home/ec2-user/IS707/flask
 bash aws_commands.sh
